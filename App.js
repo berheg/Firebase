15affa0385c16e6bf8213c455921e199454f19b5
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-12-21 22:21:52 +0100

    facebook, google and twitter login added

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import fire from './components/config/Fire';
+import {fire, facebookProvider, googleProvider, twitterProvider} from './components/config/Fire';
 import Home from './components/Home';
-import Login from './components/Login';
+import Form from './container/Form/Form.component';
+//import Loading from './components/Loading/Loading.component';
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = ({
       user: null,
+      email: '',
+      password: '',
+      isLoading: false
     });
     this.authListener = this.authListener.bind(this);
   }
@@ -19,19 +24,85 @@ class App extends Component {
 
   authListener() {
     fire.auth().onAuthStateChanged((user) => {
+      this.setState({isLoading: true});
       console.log(user);
       if (user) {
-        this.setState({ user });
+        this.setState({ user });        
         localStorage.setItem('user', user.uid);
       } else {
         this.setState({ user: null });
         localStorage.removeItem('user');
       }
+      this.setState({isLoading: false});
     });
   }
+  
+  handleChange =(e) => {
+    this.setState({ [e.target.type]: e.target.value });
+  }
+
+  login = (e) => {
+    this.setState({isLoading: true});
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+      this.setState({isLoading: false});
+    }).catch((error) => {
+        console.log(error);
+      });
+  }
+
+  signup = (e) => {
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+    }).then((u)=>{console.log(u)})
+    .catch((error) => {
+        console.log(error);
+      })
+  }
+  onGoogleSignIn = googleUser => {    
+      console.log("google authentication");
+      fire.auth().signInWithPopup(googleProvider)
+   .then((result, error) => {
+    if(error){
+      console.log(error)
+    }
+    else{
+
+    }
+   })
+  };
+ onFacebookSignIn = () => {
+   fire.auth().signInWithPopup(facebookProvider)
+   .then((result, error) => {
+    if(error){
+      console.log(error)
+    }
+    else{
+
+    }
+   })
+ }
+onTwitterSignIn = () => {
+  fire.auth().signInWithPopup(twitterProvider)
+   .then((result, error) => {
+    if(error){
+      console.log(error)
+    }
+    else{
+
+    }
+   })
+}
   render() {
     return (
-     <div>{this.state.user ?  ( <Home/>) : (<Login />)}</div>
+     <div>       
+       {this.state.user ?  (<Home/>) : 
+       (<Form 
+        onGoogleSignIn ={this.onGoogleSignIn}
+        onFacebookSignIn={this.onFacebookSignIn}
+        onTwitterSignIn={this.onTwitterSignIn}
+        login ={this.login}
+        handleChange={this.handleChange}
+       />)}
+       </div>
   )
 };
 }
