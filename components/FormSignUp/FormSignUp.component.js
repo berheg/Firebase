4af07e9201bcc45ed4bad0810b457cd4eef52bc4
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2020-01-05 22:50:12 +0100

    FormSignUp component added

---
+++ components/FormSignUp/FormSignUp.component.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import './FormSignUp.css';
+import FormButton from '../FormButton/FormButton.component';
+import FormField from '../FormField/FormField.component';
+import FormTitle from '../FormTitle/FormTitle.component';
+import FormLine from '../FormLine/FormLine.component';
+import {
+  faGoogle,
+  faFacebookSquare,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+class FormSignUp extends Component {
+  render() {
+    return (
+      <div className="signup-wrapper">
+        <div className="signup-part">
+          <FormTitle
+            formTitle="form-title"
+            text="Sign up to Annotate the web"
+          />
+          <div className="social-media-button-wrapper">
+            <FormButton
+              title="Sign up with Google"
+              buttons="google-btn"
+              logo={faGoogle}
+              align="logo-align"
+              click={this.props.onGoogleSignIn}
+            />
+
+            <FormButton
+              title=""
+              buttons="twitter-btn"
+              logo={faTwitter}
+              click={this.props.onTwitterSignIn}
+            />
+
+            <FormButton
+              title=""
+              buttons="facebook-btn"
+              logo={faFacebookSquare}
+              click={this.props.onFacebookSignIn}
+            />
+          </div>
+          <FormLine />
+          <div className="name-wrapper">
+            <FormField
+              title="Full Name"
+              text="emaill"
+              box="nameboxclass"
+              inputType="text"
+              inputClass="name-box"
+              // changeHandler={(e) => {
+              //   e.target.value;
+              // }}
+            />
+            <FormField
+              title="User Name"
+              text="emaill"
+              box="nameboxclass"
+              inputType="text"
+              inputClass="name-box"
+              // changeHandler={(e) => {
+              //   e.target.value;
+              // }}
+            />
+          </div>
+          <FormField
+            title="Email Address"
+            text="login-input-label"
+            box="boxclass"
+            inputType="email"
+            inputClass="email-addres-box"
+            changeHandler={this.props.handleChange}
+          />
+          <FormField
+            title="Password"
+            text="login-input-label"
+            box="boxclass"
+            inputType="password"
+            inputClass="password-box"
+            changeHandler={this.props.handleChange}
+          />
+          <div className="signup-button-wrapper">
+            <FormButton
+              title="Create Account"
+              buttons="submit-btn"
+              click={this.props.signup}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default FormSignUp;
